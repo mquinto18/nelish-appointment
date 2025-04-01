a700b5f9f8271ba999d05f3def35f6b8506163df
mquinto18 <quintom53@gmail.com>
2025-04-02 02:42:09 +0800

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nelish Serenity Spa - Login</title>
     @vite('resources/css/app.css')
+    <link rel="icon" type="image/png" href="{{ asset('images/nelish-logo.png') }}">
     <link rel="stylesheet" href="Login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style type="text/css">
